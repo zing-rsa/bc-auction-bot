a1721fab9970004bd434b040369ffbeab98a71f9
Robert Kritzinger <kritz.rob@gmail.com>
2021-12-09 09:54:15 +0200

    add 1min extensions

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import json
 import copy
 import discord
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 from pymongo import MongoClient
 from discord.ext import commands, tasks
 
@@ -219,6 +219,7 @@ async def bid(ctx, price: int):
 
     now = datetime.now()
     a_id = ctx.channel.id
+    extended = False
 
     if str(a_id) in auctions_list:
 
@@ -233,6 +234,10 @@ async def bid(ctx, price: int):
                 a['highBidId'] = ctx.message.author.id
                 a['highBidName'] = ctx.message.author.name
                 a['bids'] = a['bids'] + 1
+
+                if now > a['end'] - timedelta(minutes=1):
+                    a['end'] = a['end'] + timedelta(minutes=1)
+                    extended = True
 
                 for field in a['embed']['fields']:
                     if field['name'] == 'Price:':
@@ -254,6 +259,9 @@ async def bid(ctx, price: int):
                         await ctx.send(user.mention + ' You have been outbid')
                 except:
                     print("Failed mention")
+
+                if extended:
+                    await ctx.send("Auction extended by 1 min! New end time: " + str(a['end']))
 
                 org_embed_msg = await ctx.fetch_message(a['msg_id'])
 
